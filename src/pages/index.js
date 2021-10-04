f7831e2c1954c6b830d5cbb6bff510b7e02fe550
cpiment <pimentel.carlos@gmail.com>
2021-10-04 21:54:16 +0200

    Finished Part 2 of Tutorial

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,6 @@
 import * as React from 'react'
 import Layout from '../components/layout'
 
-//Next Step: https://www.gatsbyjs.com/docs/tutorial/part-2/#style-components-with-css-modules
-
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
