2343c06885b7632a959874a4e884c80293704741
cpiment <pimentel.carlos@gmail.com>
2021-10-11 18:15:07 +0200

    Added miniature to blog post list

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,6 +1,8 @@
 import * as React from 'react'
 import Layout from '../../components/layout'
 import { graphql, Link } from 'gatsby'
+import { getImage, GatsbyImage } from 'gatsby-plugin-image'
+import { article, text, image } from './index.module.css'
 
 const BlogPage = ({ data }) => {
   return (
@@ -8,14 +10,22 @@ const BlogPage = ({ data }) => {
       <ul>
       {
         data.allMdx.nodes.map((node) => (
-          <article key={node.id}>
-            <h2>
-              <Link to={`/blog/${node.slug}`}>
-                {node.frontmatter.title}
-              </Link>
-            </h2>
-            <p>Posted: {node.frontmatter.date}</p>   
-            <p>Updated: {node.frontmatter.update_date}</p>   
+          <article key={node.id} className={article}>
+            <Link to={`/blog/${node.slug}`}>
+              <GatsbyImage className={image}
+                image={getImage(node.frontmatter.hero_image)}
+                alt={node.frontmatter.hero_image_alt}
+              />
+            </Link>
+            <div className={text}>
+              <h2>
+                <Link to={`/blog/${node.slug}`}>
+                  {node.frontmatter.title}
+                </Link>
+              </h2>
+              <p>Posted: {node.frontmatter.date}</p>   
+              <p>Updated: {node.frontmatter.update_date}</p>   
+            </div>
           </article>
         ))
       }
@@ -32,6 +42,11 @@ query {
         title
         date(formatString: "MMMM DD, YYYY")
         update_date(formatString: "MMMM DD, YYYY")
+        hero_image {
+          childImageSharp {
+            gatsbyImageData(width: 150, layout: CONSTRAINED)
+          }
+        }
       }
       id
       slug
