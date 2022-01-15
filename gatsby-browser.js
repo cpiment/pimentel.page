85b85fa58aede6a02327c5aadfa49e2beda97bc6
cpiment <pimentel.carlos@gmail.com>
2022-01-15 23:27:53 +0100

    Removed duplicated code for syntax highlighting

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,16 +1,1 @@
-//Copied from https://fabiorosado.dev/blog/add-code-highlighting-to-mdx/
-//Author: Fabio Rosado (https://twitter.com/FabioRosado)
-
-import React from "react"
-import { MDXProvider } from "@mdx-js/react"
-
-import CodeBlock from "./src/components/CodeBlock"
 import "./src/styles/global.css"
-
-const component = {
-    pre: CodeBlock
-}
-
-export const wrapRootElement = ({ element }) => {
-    return <MDXProvider components={component}>{element}</MDXProvider>
-}
